2ee135b37124884450f5a467346b5fde9df2ee44
Emily Miles <emiles4@gmail.com>
2018-09-08 04:23:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,14 +72,15 @@ end
 def turn
     puts "Please enter 1-9:"
     input = gets.strip
-    index = input.to_i
+    index = input_to_index(input)
+    char = current_player
     if valid_move?(index)
-      move(index, current_player)
+      move(index, char)
       display_board
     else
       turn
     end
-  end
+end
 
  def won?
      board_empty = @board.none? { |i| i == "X" || i = "O"}
